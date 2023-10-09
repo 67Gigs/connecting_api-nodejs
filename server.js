3c8eea145c0d822cdef5193a9eeb465cd810e52f
Noureddine Ben sadok <noureddine.bensadok1@gmail.com>
2023-10-09 09:47:54 +0200

    last version, everything working, can add new features to it, ofc securing it and adding things to it, potentially connecting to a subdomain, adding subdomain users, adding new features to the database and adapting it to each website am gonna make

--- server.js
+++ server.js
@@ -5,19 +5,24 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const pg = require('pg');
 const bcrypt = require('bcrypt');
-const PORT =  process.env.PORT | 8081;
+const PORT = 8081;
 
 
 //env variables
-const DB_DATABASE = process.env.DB_DATABASE;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
+const DB_DATABASE = process.env.DATABASE;
+const DB_USER = process.env.USER;
+const DB_PASSWORD = process.env.PASS;
+const DB_HOST = process.env.HOST;
+const DB_PORT = process.env.PORT;
+
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 app.use(cookieParser());
 
 
@@ -39,9 +44,10 @@ const Pool = new pg.Pool({
 Pool.connect()
 
 
+// const todayDate = new Date().toISOString().slice(0, 10);
 // const query = {
-//     text: 'INSERT INTO users(id, name, email, password) VALUES($1, $2, $3, $4)',
-//     values: [1, 'brianc', 'brian.m.carlson@gmail.com', 'test'],
+//     text: 'INSERT INTO users(last_login, name, email, password) VALUES($1, $2, $3, $4)',
+//     values: [todayDate, 'brianc', 'brian.m.carlson@gmail.com', 'test'],
 // }
 
 // Pool.query(query, (err, res) => {
@@ -53,7 +59,35 @@ Pool.connect()
 // })
 
 
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) return res.json({ Error: 'Access Denied' });
+    jwt.verify(token, process.env.JWT_KEY, (err, result) => {
+        if (err) return res.json({ Error: 'token is not okey' });
+        req.name = result.name;
+        next();
+    });
+};
+
+
 // Routes
+
+//home route
+
+app.get('/', verifyUser, (req, res) => {
+    return res.json({ Status: 'Success', name: req.name });
+});
+
+app.get('/login', verifyUser, (req, res) => {
+    return res.json({ Status: 'Success', name: req.name });
+})
+
+app.get('/register', verifyUser, (req, res) => {
+    return res.json({ Status: 'Success', name: req.name });
+})
+
+
+//register route
 
 app.post('/api/register', (req, res) => {
 
@@ -70,12 +104,44 @@ app.post('/api/register', (req, res) => {
 
         Pool.query(query, (err, result) => {
             if (err) return res.status(500).json({ Error: err });
-            return res.status(200).json({ Success: 'Success' });
+            return res.status(200).json({ Status: 'Success' });
         })
     });
 
-    
-
-    
 });
 
+
+//login route
+
+app.post('/api/login', (req, res) => {
+    //querry database for email
+    const querry = {
+        text: 'SELECT * FROM users WHERE email = $1',
+        values: [req.body.email]
+    };
+
+    //querring database and sending sql request
+    Pool.query(querry, (err, result) => {
+        if (err) return res.status(500).json({ Error: err }); //if error return error
+        if (result.rows.length < 1) return res.json({ Error: 'No account found' }); //if no account found return error
+
+        bcrypt.compare(req.body.password.toString(), result.rows[0].password, (err, response) => {
+            if (err) return res.status(401).json({ Error: 'Password compare error' }); //if error return error for compare
+
+            if (response) {
+                const name = result.rows[0].name;
+                const email = result.rows[0].email;
+                const token = jwt.sign({ name, email }, process.env.JWT_KEY, { expiresIn: '1h' }); //create token for cookie
+                res.cookie('token', token); //set cookie
+                return res.status(200).json({ Status: 'Success' }); //if password match return success
+            }else {
+                return res.json({ Error: 'Password Not match' });
+            }
+        });
+    });
+});
+
+app.get('/api/logout', (req, res) => {
+    res.clearCookie('token');
+    return res.status(200).json({ Status: 'Success' });
+})
