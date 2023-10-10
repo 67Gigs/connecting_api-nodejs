274e96d3b91a2d813bd738459e4d5ab4860ed5ec
Noureddine Ben sadok <noureddine.bensadok1@gmail.com>
2023-10-10 14:04:09 +0200

    added reset password and account delete, works as a beauty, need to secure more the token, and for delete maybe i will have to add remove via email to make sure its the client that is deleting his account

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const pg = require('pg');
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer')
 const PORT = 8081;
 
 
@@ -43,6 +44,16 @@ const Pool = new pg.Pool({
 
 Pool.connect()
 
+
+const Client = new pg.Client({
+    host: DB_HOST,
+    user: DB_USER,
+    password: 'NouNou23',
+    port: DB_PORT,
+    database: DB_DATABASE
+});
+
+Client.connect()
 
 // const todayDate = new Date().toISOString().slice(0, 10);
 // const query = {
@@ -110,7 +121,6 @@ app.post('/api/register', (req, res) => {
 
 });
 
-
 //login route
 
 app.post('/api/login', (req, res) => {
@@ -131,8 +141,17 @@ app.post('/api/login', (req, res) => {
             if (response) {
                 const name = result.rows[0].name;
                 const email = result.rows[0].email;
-                const token = jwt.sign({ name, email }, process.env.JWT_KEY, { expiresIn: '1h' }); //create token for cookie
+                const token = jwt.sign({ name, email }, process.env.JWT_KEY, { expiresIn: '1d' }); //create token for cookie
                 res.cookie('token', token); //set cookie
+                const date = new Date().toISOString().slice(0, 10);
+                values = [date, email];
+                const query = {
+                    text: 'UPDATE users SET last_login = $1 WHERE email = $2',
+                    values: values
+                };
+                Pool.query(query, (err, result) => {
+                    if (err) return res.status(500).json({ Error: err });
+                });
                 return res.status(200).json({ Status: 'Success' }); //if password match return success
             }else {
                 return res.json({ Error: 'Password Not match' });
@@ -141,7 +160,107 @@ app.post('/api/login', (req, res) => {
     });
 });
 
+// logout route
+
 app.get('/api/logout', (req, res) => {
     res.clearCookie('token');
     return res.status(200).json({ Status: 'Success' });
+})
+
+// delete account route
+
+app.get('/api/delete', (req, res) => {
+    const token = req.cookies.token;
+    if (!token) return res.json({ Error: 'Access Denied' });
+    jwt.verify(token, process.env.JWT_KEY, (err, result) => {
+        if (err) return res.json({ Error: 'token is not okey' });
+        req.email = result.email;
+    });
+
+    const query = {
+        text: 'DELETE FROM users WHERE email = $1',
+        values: [req.email]
+    }
+    Pool.query(query, (err, result) => {
+        if (err) return res.status(500).json({ Error: err });
+    });
+
+    res.clearCookie('token');
+    return res.status(200).json({ Status: 'Success' });
+})
+
+
+// forgot password route
+
+app.post('/forgot-password', (req, res) => {
+    const {email} = req.body;
+    const query = {
+        text: 'SELECT * FROM users WHERE email = $1',
+        values: [email]
+    }
+    Pool.query(query, (err, result) => {
+        console.log(result.length);
+        if(err) {
+            return res.send({Status: "User not existed"})
+        }
+        if (result.rows.length < 1) {
+            return res.send({Status: "User doesn't exist"});
+        }
+        const token = jwt.sign({id: email}, process.env.JWT_KEY, {expiresIn: "1d"})
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
+            auth: {
+              user: 'noureddine.api@gmail.com',
+              pass: 'jdmgfgjsanlphqlv'
+            }
+          });
+          
+          var mailOptions = {
+            from: 'noureddine.api@gmail.com',
+            to: email,
+            subject: 'Reset Password Link',
+            text: `http://localhost:3000/reset_password/${email}/${token}`
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              return res.send({Status: "Success"})
+            }
+          });
+    })
+})
+
+//reset password route
+
+app.post('/reset-password/:email/:token', (req, res) => {
+    const {email, token} = req.params
+    const {password} = req.body
+
+    jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+        if(err) {
+            return res.json({Status: "Error with token"})
+        } else {
+            bcrypt.hash(password, 10)
+            .then(hash => {
+                const query = {
+                    text: "UPDATE users SET password = $1  WHERE email = $2",
+                    values: [hash, email]
+                };
+
+                Pool.query(query, (err, result) => {
+                    if(err) {
+                        return res.send({Status: "User not existed"})
+                    }
+                    return res.send({Status: "Success"});
+                })
+
+            })
+            .catch(err => res.send({Status: err}))
+        }
+    })
 })
